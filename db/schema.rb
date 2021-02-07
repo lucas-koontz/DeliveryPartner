ff678d9c8f376f62016f40923d18ecc6b48bb968
Lucas Koontz <lucas.emanuel19@gmail.com>
2021-02-07 17:08:19 -0300

    fix: add missing schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,20 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_206_204_242) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'pgcrypto'
-  enable_extension 'plpgsql'
-  enable_extension 'postgis'
+ActiveRecord::Schema.define(version: 2021_02_06_204242) do
 
-  create_table 'partners', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
-    t.string 'trading_name', null: false
-    t.string 'owner_name', null: false
-    t.string 'document', null: false
-    t.geometry 'coverage_area', limit: { srid: 0, type: 'multi_polygon' }, null: false
-    t.geometry 'address', limit: { srid: 0, type: 'st_point' }, null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['document'], name: 'index_partners_on_document', unique: true
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
+  enable_extension "plpgsql"
+  enable_extension "postgis"
+
+  create_table "partners", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "trading_name", null: false
+    t.string "owner_name", null: false
+    t.string "document", null: false
+    t.geometry "coverage_area", limit: {:srid=>0, :type=>"multi_polygon"}, null: false
+    t.geometry "address", limit: {:srid=>0, :type=>"st_point"}, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["document"], name: "index_partners_on_document", unique: true
   end
+
 end
